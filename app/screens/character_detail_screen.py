fef54032c92f95f4d140c622d6f480165df96fe3
Just-trying-my-luck <natalie.alfz@gmail.com>
2026-09-12 08:30:16 -0500

    json doc linked to main.py

--- app/screens/character_detail_screen.py
+++ app/screens/character_detail_screen.py
@@ -28,6 +28,51 @@ class CharacterDetailScreen:
 			font=("Helvetica", 16)
 		)
 		birth_date.pack(pady=10)
+		
+		physical_title = tk.Label(
+			self.frame,
+			text="PHYSICAL",
+			font=("Helvetica", 20, "bold")
+		)
+		physical_title.pack(pady=(20, 10))
+		
+		physical = self.character["physical"]
+		
+		height = tk.Label(
+			self.frame,
+			text=f"Height: {physical['height']}",
+			font=("Helvetica", 14)
+		)
+		height.pack(pady=2)
+		
+		build = tk.Label(
+			self.frame,
+			text=f"Build: {physical['build']}",
+			font=("Helvetica", 14)
+		)
+		build.pack(pady=2)
+		
+		hair = tk.Label(
+			self.frame,
+			text=f"Hair: {physical['hair']}",
+			font= ("Helvetica", 14)
+		)
+		hair.pack(pady=2)
+		
+		eyes = tk.Label(
+			self.frame,
+			text=f"Eyes: {physical['eyes']}",
+			font=("Helvetica", 14)
+		)
+		eyes.pack(pady=2)
+		
+		features = tk.Label(
+			self.frame,
+			text=f"Distinguishing features: {physical['distinguishing_features']}",
+			font=("Helvetica", 14)
+		)
+		features.pack(pady=2)
+		
 		back_button = tk.Button(
 			self.frame,
 			text="Back",
